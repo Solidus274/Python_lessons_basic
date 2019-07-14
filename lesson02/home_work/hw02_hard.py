1557dc55e25b6e6d99175a94106c1675d8d66fbf
Solidus274 <51530091+Solidus274@users.noreply.github.com>
2019-07-14 20:45:05 +0300

    Выполнены задания уровня hard

--- lesson02/home_work/hw02_hard.py
+++ lesson02/home_work/hw02_hard.py
@@ -1,10 +1,14 @@
 # Задание-1: уравнение прямой вида y = kx + b задано в виде строки.
 # Определить координату y точки с заданной координатой x.
-
-equation = 'y = -12x + 11111140.2121'
-x = 2.5
 # вычислите и выведите y
 
+equation = 'y = -12x + 11111140.2121'
+
+x = 2.5
+
+y = float(equation[4:equation.find("x")]) * x + float(equation[11:])
+
+print(y)
 
 # Задание-2: Дата задана в виде строки формата 'dd.mm.yyyy'.
 # Проверить, корректно ли введена дата.
@@ -17,13 +21,32 @@ x = 2.5
 #  (т.е. 2 символа для дня, 2 - для месяца, 4 - для года)
 
 # Пример корректной даты
-date = '01.11.1985'
+# date = '01.11.1985'
 
 # Примеры некорректных дат
-date = '01.22.1001'
-date = '1.12.1001'
-date = '-2.10.3001'
+# date = '01.22.1001'
+# date = '1.12.1001'
+# date = '-2.10.3001'
 
+date = str(input('Введите дату в формате: dd.mm.yyyy '))
+list_date = date.split('.')
+day = int(list_date[0])
+month = int(list_date[1])
+year = int(list_date[2])
+long_month = [1, 3, 5, 7, 8, 10, 12]
+
+if date.find(".") != 2 or date.find(".", 3) != 5 or len(date) != 10:
+    print('Дата введена не корректно')
+elif day < 1 or day > 31:
+    print('День введен не корректно')
+elif month < 1 or month > 12:
+    print('Месяц введен не корректно')
+elif year < 1 or year > 9999:
+    print('Год введен не корректно')
+elif month not in long_month and day > 30:
+    print('День введен не корректно')
+else:
+    print('Дата введена корректно:', date)
 
 # Задание-3: "Перевёрнутая башня" (Задача олимпиадного уровня)
 #
@@ -54,3 +77,31 @@ date = '-2.10.3001'
 #
 # Вход: 11
 # Выход: 5 3
+
+N = int(input('Введите номер комнаты:'))
+
+floor = 1
+room = 1
+answer_floor = 0
+answer_room = 0
+
+if N <= 0 or N > 2000000000:
+    print(f'Комнаты с номером {N} нет в нашем отеле. Возможно, вы ошиблись?')
+else:
+    while 1 <= N <= 2000000000:
+        for current_floor in range(floor):
+            answer_floor += 1
+            for current_room in range(room):
+                N -= 1
+                if N == 0:
+                    answer_room = current_room + 1
+                    break
+            if N == 0:
+                break
+        floor += 1
+        room += 1
+    if answer_floor == 1:
+        print(f'Комната расположена на {answer_floor} этаже. Это единственная комната на этаже. Не заблудитесь.')
+    else:
+        print(f'Комната расположена на {answer_floor} этаже, {answer_room}-я слева.')
+        
