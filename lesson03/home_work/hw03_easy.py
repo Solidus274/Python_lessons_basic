7d1226151c8ae3ab7cfce11890e9a05e0eb7e4cb
Solidus274 <51530091+Solidus274@users.noreply.github.com>
2019-07-17 23:36:15 +0300

    Выполнены задания уровня easy

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -5,7 +5,18 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    number = str(number)
+    number = number.split(".")
+    if int(number[1][ndigits]) <= 4:
+        number[1] = number[1][:ndigits]
+    else:
+        if int(number[1][0]) == 9:
+            number[0] = str(int(number[0]) + 1)
+            number[1] = str(int(number[1][:ndigits]) + 1)[1:]
+        else:
+            number[1] = str(int(number[1][:ndigits]) + 1)
+    number = float(number[0] + "." + number[1])
+    return number
 
 
 print(my_round(2.1234567, 5))
@@ -20,7 +31,17 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    ticket = str(ticket_number)
+    tekcit = ticket[::-1]
+    sum_1 = 0
+    sum_2 = 0
+    for x in range (int(len(ticket) / 2)):
+        sum_1 += int(ticket[0 + x])
+        sum_2 += int(tekcit[0 + x])
+    if sum_1 == sum_2:
+        return 'Счастливый'
+    else:
+        return 'Обычный'
 
 
 print(lucky_ticket(123006))
