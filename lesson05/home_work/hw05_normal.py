dcc0cf8bfadb913687da10a200f466f7f7291dcb
Solidus274 <51530091+Solidus274@users.noreply.github.com>
2019-07-25 13:07:05 +0300

    Выполнены задания уровня normal

--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,46 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import os
+import sys
+import shutil
+import hw05_easy as easy
+
+print('sys.argv = ', sys.argv)
+
+
+def print_help():
+    print('help - получение справки')
+    print("change_dir <path> - перемещение в указанную директорию")
+    print('list_dir - отображение папок находящихся в текущей директории')
+    print('del_folder <path>  - удаление директории по указанному пути')
+    print('make_folder <path>  - создание директории по указанному пути')
+
+
+
+do = {
+    "help": print_help,
+    "make_folder": easy.make_folder,
+    "del_folder": easy.del_folder,
+    "list_dir": easy.list_dir,
+    "change_dir": easy.change_dir
+    }
+
+
+try:
+    stringpath = sys.argv[2]
+except IndexError:
+    stringpath = None
+
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = None
+
+if key:
+    if do.get(key):
+        do[key]()
+    else:
+        print("Задан неверный ключ")
+        print("Укажите ключ help для получения справки")
